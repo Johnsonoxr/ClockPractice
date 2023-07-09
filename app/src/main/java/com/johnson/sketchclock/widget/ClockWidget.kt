0fa1dee425d5b740d9408a7260256989b4c6dcde
Johnson1 Chang <Johnson1_Chang@asus.com>
2023-07-09 16:48:17 +0800

    Forget to setup the AlarmManager. STUPID.

--- app/src/main/java/com/johnson/sketchclock/widget/ClockWidget.kt
+++ app/src/main/java/com/johnson/sketchclock/widget/ClockWidget.kt
@@ -231,6 +231,7 @@ class ClockWidget : AppWidgetProvider() {
                 }
                 postNextMinuteUpdate(context)
                 updateWidgetImage(context, forceUpdate = intent.getBooleanExtra(EXTRA_FORCE_UPDATE, false))
+                setupAlarmManager(context)
                 if (intent.getBooleanExtra(EXTRA_CLICK_EFFECT, false)) {
                     performClickAnimation(context)
                 }
