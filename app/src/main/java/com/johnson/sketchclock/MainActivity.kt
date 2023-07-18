a8f6a652661c70bdc2616ecd50ea5511c18a9dd2
Johnson1 Chang <Johnson1_Chang@asus.com>
2023-07-18 21:34:03 +0800

    Complete BoundaryParser. Now how can I put it into the project?

--- app/src/main/java/com/johnson/sketchclock/MainActivity.kt
+++ app/src/main/java/com/johnson/sketchclock/MainActivity.kt
@@ -1,22 +1,11 @@
 package com.johnson.sketchclock
 
 import android.content.Intent
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
-import android.graphics.Path
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.lifecycleScope
-import com.johnson.sketchclock.common.BoundaryParser
 import com.johnson.sketchclock.databinding.ActivityMainBinding
 import com.johnson.sketchclock.pickers.PickersActivity
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlin.system.measureNanoTime
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -30,33 +19,6 @@ class MainActivity : AppCompatActivity() {
 
         vb.fab.setOnClickListener {
             startActivity(Intent(this, PickersActivity::class.java))
-        }
-
-        val bitmap = assets.open("default_stickers/2/sticker.png").use {
-            val bmp = BitmapFactory.decodeStream(it, null, BitmapFactory.Options().apply { inMutable = true })!!
-            Bitmap.createBitmap(bmp, 500, 0, 200, 200)
-        }!!
-        vb.imageView.setImageBitmap(bitmap)
-
-        lifecycleScope.launch(Dispatchers.Default) {
-            var path: Path?
-            measureNanoTime {
-                path = BoundaryParser.parseBoundary(bitmap)
-            }.let { println("parseBoundary took ${it / 1e6} ms") }
-
-            val nnPath = path ?: return@launch
-
-            val paint = Paint().apply {
-                color = Color.RED
-                style = Paint.Style.STROKE
-                strokeWidth = 2f
-            }
-
-            Canvas(bitmap).drawPath(nnPath, paint)
-
-            launch(Dispatchers.Main) {
-                vb.imageView.setImageBitmap(bitmap)
-            }
         }
     }
 }
--- app/src/main/java/com/johnson/sketchclock/common/BoundaryParser.kt
+++ app/src/main/java/com/johnson/sketchclock/common/BoundaryParser.kt
@@ -5,50 +5,66 @@ import android.graphics.Path
 import android.graphics.Point
 import androidx.core.graphics.alpha
 
+private const val TAG = "BoundaryParser"
+
 object BoundaryParser {
 
-    fun parseBoundary(bitmap: Bitmap): Path? {
+    fun parseBoundary(bitmap: Bitmap): Path {
+        val pathCollection = Path()
+        val edgeMask = Array(bitmap.height) { BooleanArray(bitmap.width) }
 
-        val nonTransparentMat = diffuse(bitmap)
-        val edgePoints = squareTracking(nonTransparentMat)
+        val nonTransparentMat: Array<BooleanArray> = diffuse(bitmap)
 
-        val path = Path().apply {
-            moveTo(edgePoints.first().x.toFloat(), edgePoints.first().y.toFloat())
-            edgePoints.forEach { point ->
-                lineTo(point.x.toFloat(), point.y.toFloat())
+        while (true) {
+            val edgePoints: List<Point> = squareTracking(nonTransparentMat, edgeMask)
+
+            if (edgePoints.isEmpty()) {
+                break
             }
-            close()
+
+            edgePoints.forEach { point ->
+                edgeMask[point.y][point.x] = true
+            }
+
+            val path = Path().apply {
+                moveTo(edgePoints.first().x.toFloat(), edgePoints.first().y.toFloat())
+                edgePoints.forEach { point ->
+                    lineTo(point.x.toFloat(), point.y.toFloat())
+                }
+                close()
+            }
+            pathCollection.addPath(path)
         }
-        return path
+
+        return pathCollection
     }
 
     private fun diffuse(bitmap: Bitmap, steps: Int = 5): Array<BooleanArray> {
-        val pixels = IntArray(bitmap.width * bitmap.height)
-        val distMat = Array(bitmap.height) { IntArray(bitmap.width) }
-        bitmap.getPixels(pixels, 0, bitmap.width, 0, 0, bitmap.width, bitmap.height)
+        val width = bitmap.width
+        val height = bitmap.height
 
-        for (y in 0 until bitmap.height) {
-            for (x in 0 until bitmap.width) {
-                distMat[y][x] = when (pixels[y * bitmap.width + x].alpha) {
-                    0 -> Int.MAX_VALUE
-                    else -> 0
-                }
+        val pixels = IntArray(width * height)
+        bitmap.getPixels(pixels, 0, width, 0, 0, width, height)
+
+        for (i in pixels.indices) {
+            pixels[i] = when (pixels[i].alpha) {
+                0 -> Int.MAX_VALUE
+                else -> 0
             }
         }
 
-        for (dist in 0 until steps - 1) {
-            for (y in distMat.indices) {
-                for (x in 0 until distMat[y].size) {
-                    if (distMat[y][x] != Int.MAX_VALUE) continue
+        for (step in 0 until steps) {
+            for (y in 0 until height) {
+                for (x in 0 until width) {
+                    val index = y * width + x
+                    if (pixels[index] != Int.MAX_VALUE) continue
 
-                    val left = if (x > 0) distMat[y][x - 1] else Int.MAX_VALUE
-                    val top = if (y > 0) distMat[y - 1][x] else Int.MAX_VALUE
-                    val right = if (x < distMat[y].size - 1) distMat[y][x + 1] else Int.MAX_VALUE
-                    val bottom = if (y < distMat.size - 1) distMat[y + 1][x] else Int.MAX_VALUE
-
-                    val minDist = minOf(left, top, right, bottom)
-                    if (minDist == dist) {
-                        distMat[y][x] = dist + 1
+                    if (x > 0 && pixels[index - 1] == step
+                        || y > 0 && pixels[index - width] == step
+                        || x < width - 1 && pixels[index + 1] == step
+                        || y < height - 1 && pixels[index + width] == step
+                    ) {
+                        pixels[index] = step + 1
                     }
                 }
             }
@@ -57,24 +73,30 @@ object BoundaryParser {
         val mat = Array(bitmap.height) { BooleanArray(bitmap.width) }
         for (y in mat.indices) {
             for (x in 0 until mat[y].size) {
-                mat[y][x] = distMat[y][x] != Int.MAX_VALUE
+                mat[y][x] = pixels[y * width + x] != Int.MAX_VALUE
             }
         }
 
         return mat
     }
 
-    private fun squareTracking(mat: Array<BooleanArray>): List<Point> {
+    /**
+     * @param mat The boolean 2d array to perform square tracking on
+     * @param invalidStartPointMask The boolean 2d array to use as a mask for the starting point
+     */
+    private fun squareTracking(mat: Array<BooleanArray>, invalidStartPointMask: Array<BooleanArray>): List<Point> {
         val width = mat[0].size
         val height = mat.size
 
         val pts = mutableListOf<Point>()
         val pt = Point(Int.MAX_VALUE, Int.MAX_VALUE)
 
-        for (y in 0 until height) {
+        for (y in -1 until height - 1) {
+            val row = mat.getOrNull(y)
+            val row1 = mat[y + 1]
             for (x in 0 until width) {
-                if (mat[y][x]) {
-                    pt.set(x, y - 1)
+                if (row1[x] && row?.get(x) != true && !invalidStartPointMask[y][x]) {
+                    pt.set(x, y)
                     break
                 }
             }
@@ -94,9 +116,6 @@ object BoundaryParser {
             } else {
                 pt.offset(direction)
                 direction.turnLeft()
-            }
-            if (pts.size > 2000) {
-                break
             }
         } while (pt != startPt || direction != startDirection)
 
